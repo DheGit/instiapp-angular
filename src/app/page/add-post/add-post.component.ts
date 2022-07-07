074c20171bba6023cb9516142ab74345a0954a21
Vedant Thakre <44766005+vedant-github@users.noreply.github.com>
2022-07-07 20:38:14 +0530

    create_post: added profile info

--- src/app/page/add-post/add-post.component.ts
+++ src/app/page/add-post/add-post.component.ts
@@ -1,16 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
 
+const DEFAULT_USERNAME = 'Guest';
+const DEFAULT_LDAP = 'IITB User';
+const DEFAULT_PROFILE_PIC = 'assets/useravatar.svg';
+
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
   styleUrls: ['./add-post.component.css']
 })
 export class AddPostComponent implements OnInit {
-
-  tagged: String[];
-  taggables: String[];
-
+  public userName = DEFAULT_USERNAME;
+  public ldap = DEFAULT_LDAP;
+  public profilePic = DEFAULT_PROFILE_PIC;
   constructor(
     public dataService: DataService,
   ) { }
